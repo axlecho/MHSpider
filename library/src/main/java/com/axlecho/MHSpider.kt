2445f20e60978d6529b8bf21842e63c3f002fd1d
axlecho <axlecho@gmail.com>
2019-09-13 20:47:35 +0800

    [feature] add queue

--- library/src/main/java/com/axlecho/MHSpider.kt
+++ library/src/main/java/com/axlecho/MHSpider.kt
@@ -1,13 +1,81 @@
 package com.axlecho
 
-class MHSpiderQueue {
-    fun size(): Int {
-        return 0
+import android.util.Log
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
+
+const val TAG = "MHSpider"
+
+class MHSpiderInfo(private val url: String) {
+    private val status = "INIT"
+    private var process = -1.0f
+    private var size = -1
+    private var file = File.createTempFile(".jpg", "tmp ")
+    private lateinit var handler: Disposable
+
+    private fun InputStream.copyTo(out: OutputStream, onCopy: (totalBytesCopied: Long, bytesJustCopied: Int) -> Any): Long {
+        var bytesCopied: Long = 0
+        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+        var bytes = read(buffer)
+        while (bytes >= 0) {
+            out.write(buffer, 0, bytes)
+            bytesCopied += bytes
+            onCopy(bytesCopied, bytes)
+            bytes = read(buffer)
+        }
+        return bytesCopied
     }
 
-    fun dump() {}
+    fun excute() {
+        handler = MHNetwork.INSTANCE.get(url).subscribeOn(Schedulers.io())
+            .doOnComplete {
 
-    fun start() {}
+            }
+            .subscribe({ response ->
+                size = response.byteStream().available()
+                file.outputStream().use {
+                    response.byteStream().copyTo(it) { totalBytesCopied, _ ->
+                        process = totalBytesCopied.toFloat() / size.toFloat() * 100f
+                        return@copyTo process
+                    }
+                }
+            }, {
+
+            })
+    }
+
+    fun dump(): String {
+        return "$process  $size  ${file.absolutePath}"
+    }
+}
+
+class MHSpiderQueue(data: List<String>) {
+    private val queue = mutableListOf<MHSpiderInfo>()
+
+    init {
+        for (url in data) {
+            queue.add(MHSpiderInfo(url))
+        }
+    }
+
+    fun size(): Int {
+        return queue.size
+    }
+
+    fun dump() {
+        Log.v(TAG, "===========================================")
+        for (info in queue) {
+            Log.v(TAG, info.dump())
+        }
+        Log.v(TAG, "===========================================")
+    }
+
+    fun start() {
+        queue[0].excute()
+    }
 
     fun seekTo(index: Int) {}
 }
@@ -15,7 +83,7 @@ class MHSpiderQueue {
 class MHSpider {
     companion object {
         fun queue(data: List<String>): MHSpiderQueue {
-            return MHSpiderQueue()
+            return MHSpiderQueue(data)
         }
     }
 }
--- library/src/test/java/com/axlecho/MHNetworkTest.kt
+++ library/src/test/java/com/axlecho/MHNetworkTest.kt
@@ -9,9 +9,25 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.shadows.ShadowLog
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
 
 @RunWith(RobolectricTestRunner::class)
 class MHNetworkTest {
+
+    fun InputStream.copyTo(out: OutputStream, onCopy: (totalBytesCopied: Long, bytesJustCopied: Int) -> Any): Long {
+        var bytesCopied: Long = 0
+        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+        var bytes = read(buffer)
+        while (bytes >= 0) {
+            out.write(buffer, 0, bytes)
+            bytesCopied += bytes
+            onCopy(bytesCopied, bytes)
+            bytes = read(buffer)
+        }
+        return bytesCopied
+    }
 
     @Before
     fun setUp() {
@@ -20,9 +36,11 @@ class MHNetworkTest {
 
     @Test
     fun testGet() {
-        val ret = MHNetwork.INSTANCE.get("http://hiphotos.baidu.com/feed/pic/item/6f061d950a7b020832cac7c06ed9f2d3572cc894.gif").subscribeOn(Schedulers.io()).subscribe {
-            Log.d("NetworkTest", "network call back")
-        }
+        val ret =
+            MHNetwork.INSTANCE.get("http://hiphotos.baidu.com/feed/pic/item/6f061d950a7b020832cac7c06ed9f2d3572cc894.gif")
+                .subscribeOn(Schedulers.io()).subscribe {
+                    Log.d("NetworkTest", "network call back")
+                }
 
         for (i in 1..100) {
 
@@ -36,11 +54,29 @@ class MHNetworkTest {
     }
 
     @Test
+    fun testDownload() {
+        val ret = MHNetwork.INSTANCE.get("https://flv2.bn.netease.com/videolib1/1810/12/mMEjO280t/SD/mMEjO280t-mobile.mp4").blockingFirst()
+        val file = File.createTempFile(".mp4", "tmp")
+        file.outputStream().use {
+            ret.byteStream().copyTo(it, onCopy = { totalBytesCopied, _ ->
+                Log.d("NetworkTest", "download $totalBytesCopied")
+            })
+        }
+
+        Assert.assertTrue(file.exists())
+        Assert.assertTrue(file.isFile)
+        Assert.assertTrue(file.length() > 0L)
+    }
+
+    @Test
     fun testData() {
         val result = ManhuaduiApi.INSTANCE.data("huiyedaxiaojiexiangrangwogaobai", "178538").blockingFirst()
         Log.d("test", result.toString())
         Assert.assertEquals(23, result.data.size)
-        Assert.assertEquals("https://mhcdn.manhuazj.com/ManHuaKu/h/huiyedaxiaojiexiangrangwogaobaitiancaimendelianait/1/201944768.jpg", result.data[0])
+        Assert.assertEquals(
+            "https://mhcdn.manhuazj.com/ManHuaKu/h/huiyedaxiaojiexiangrangwogaobaitiancaimendelianait/1/201944768.jpg",
+            result.data[0]
+        )
     }
 
     @Test
@@ -51,9 +87,12 @@ class MHNetworkTest {
         queue.dump()
 
         queue.start()
-        queue.dump()
+        for (i in 1..20) {
+            queue.dump()
+            Thread.sleep(2 * 1000)
+        }
 
-        queue.seekTo(queue.size() / 2)
-        queue.dump()
+        // queue.seekTo(queue.size() / 2)
+        // queue.dump()
     }
 }
